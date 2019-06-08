ac6d4bf2383e3b251008a0f7b9fcddeea8c271cf
Urimubenshi Daniel <benshidanny11@gmail.com>
2019-06-08 09:53:59 +0200

    Added hod daschboard chore

--- UI/js/main.js
+++ UI/js/main.js
@@ -21,10 +21,10 @@ if(email === "benshidanny11@gmail.com" && password === "Danny123" ){
     window.location.href = '../html/admin/dashboard.html';
     }
     else if(email ==="emmylavie3@gmail.com" && password === "dukuze"){
-    window.location.href = '../html/user/hod/dashboard.html'; 
+    window.location.href = '../html/hod/dashboard.html'; 
     }
-    else if(email ==="gprestein@gmail.com" && password === "eric123"){
-    window.location.href = '../html/user/lectuler/dashboard.html'; 
+    else if(email ==="gprestein55@gmail.com" && password === "eric123"){
+    window.location.href = '../html/lectuler/dashboard.html'; 
     }
     else{
     document.getElementById("incorrect").innerHTML="Inorrect email or password!"
