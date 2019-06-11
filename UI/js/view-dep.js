339d6be9e087198162bec8effa3063ee56684d3e
Urimubenshi Daniel <benshidanny11@gmail.com>
2019-06-11 22:23:33 +0200

    Added view department feature

---
+++ UI/js/view-dep.js
@@ -0,0 +1,22 @@
+function dropdown(){
+    document.getElementById("myDropdown").classList.toggle("show");
+
+}
+function hideDropdown(){
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+}
+document.getElementById("bnt-back-home-n").addEventListener('click',() =>{
+    window.location.href = '../../html/admin/dashboard.html';
+});
+document.body.onclick = function (ev) {
+if (ev.target.getAttribute("class") == "btn-add-level") {
+  window.location.href = '../../html/admin/add-lev.html';
+}
+};
